b731df0991828dfe600f0302500dc127f6291e41
Enzo Giacomini <203458225+EnzoGiacomini@users.noreply.github.com>
2026-04-17 10:27:33 -0300

    feat: criei as divisões base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <main>Hello World!</main>;
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 bg-zinc-950 text-white">
+      <div className="flex flex-col justify-center items-center p-8"></div>
+      <div className="hidden md:flex flex-col justify-center items-center bg-zinc-900"></div>
+    </div>
+  );
 }
