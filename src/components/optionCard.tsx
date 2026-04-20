aa45aa7c6d611aec0c9b5265c0132f3f958e54cc
Enzo Giacomini <enzo-giacomini@hotmail.com>
2026-04-20 18:51:56 -0300

    styles: arrumei uma falta de alinhamento nos card

--- src/components/optionCard.tsx
+++ src/components/optionCard.tsx
@@ -12,13 +12,13 @@ interface OptionCardProps {
 export function OptionCard({ name, about, arrList, icon, children }: OptionCardProps)
 {
     return(
-        <div className="max-w-125 min-h-95 bg-white w-full border-2 border-amber-600 rounded-2xl flex flex-col items-start justify-center p-5 gap-2">
+        <div className="max-w-125 bg-white w-full h-full border-2 border-amber-600 rounded-2xl flex flex-col items-start p-6 gap-2">
             <div>
                 {icon}
             </div>
             <h3 className="text-amber-600 font-bold text-[24px]">{name}</h3>
             <p className="text-gray-400 font-medium">{about}</p>
-            <ul className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-2 flex-1 w-full">
                 {arrList.map((item, index) => (
                     <li key={index} className="text-sm text-gray-800 flex items-center text-nowrap gap-2">
                         <Check className="w-6 h-6 text-orange-500 shrink-0 mt-0.5 " />
@@ -26,7 +26,7 @@ export function OptionCard({ name, about, arrList, icon, children }: OptionCardP
                     </li>
                 ))}
             </ul>
-            <div className="w-full mt-4 flex items-center justify-center">
+            <div className="w-full mt-auto pt-4 flex items-center justify-center">
                 {children}
             </div>
         </div>
