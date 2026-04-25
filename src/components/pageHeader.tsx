66a242fefca2582a0db856bd45a41ae60c806304
Enzo Giacomini <enzo-giacomini@hotmail.com>
2026-04-25 20:16:15 -0300

    styles: adc animacao ao botao e fiz o componente ser responsivo

--- src/components/pageHeader.tsx
+++ src/components/pageHeader.tsx
@@ -13,13 +13,13 @@ interface PageHeaderProps{
 export function PageHeader({title, description, hideButton, buttonText}: PageHeaderProps)
 {
     return (
-        <section className="flex flex-row items-center justify-between max-w-[80%] w-full">
-            <div className="flex flex-col gap-3">
+        <section className="flex flex-col min-[800px]:flex-row items-center gap-8 min-[800px]:gap-0 justify-between min-[500px]:max-w-[80%] w-full">
+            <div className="flex flex-col gap-3 text-center min-[800px]:text-start">
                 <h2 className="text-[42px] text-orange-500 font-bold">{title}</h2>
                 <p className="text-[18px] text-gray-500">{description}</p>
             </div>
             <div className="flex items-center justify-center">
-                <button className={`${hideButton} items-center justify-center bg-orange-500 px-6 flex flex-row py-3 text-nowrap rounded-md text-white gap-2`}><Plus className="h-8 w-8"></Plus> {buttonText}</button>
+                <button className={`${hideButton} items-center justify-center bg-orange-500 px-6 flex flex-row py-3 text-nowrap rounded-md text-white gap-2 cursor-pointer hover:bg-orange-600 transition-all active:scale-95`}><Plus className="h-8 w-8"></Plus> {buttonText}</button>
             </div>
         </section>
     );
