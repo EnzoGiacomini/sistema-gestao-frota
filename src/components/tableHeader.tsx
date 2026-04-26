ca5ff89f610d651fd450558d44a8c72352d81c63
Enzo Giacomini <enzo-giacomini@hotmail.com>
2026-04-25 23:36:23 -0300

    feat: cabeçalho da tabela pronto

---
+++ src/components/tableHeader.tsx
@@ -0,0 +1,16 @@
+export function TableHeader() {
+  return (
+    <thead className="bg-gray-100 border-b border-gray-200 text-gray-500 text-xs font-bold uppercase tracking-wider text-left w-full">
+      <tr>
+        <th className="p-4 rounded-tl-2xl text-center">Número</th>
+        <th className="p-4">Data</th>
+        <th className="p-4">Placa / Veículo</th>
+        <th className="p-4 text-center">Status</th>
+        <th className="p-4 text-center">Problemas</th>
+        <th className="p-4 text-center">Peças</th>
+        <th className="p-4 text-center">Valor</th>
+        <th className="p-4 rounded-tr-2xl text-center">Ações</th>
+      </tr>
+    </thead>
+  );
+}
