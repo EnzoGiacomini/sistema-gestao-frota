c775853bc6bfb1af0e22f782e4e9ed79ad5e2ae1
Enzo Giacomini <enzo-giacomini@hotmail.com>
2026-04-28 23:14:34 -0300

    feat: resumo de veículo finalizada

---
+++ src/app/admin/vehicles/page.tsx
@@ -0,0 +1,10 @@
+import { VehicleResume } from "@/components/vehiclePageComponents/vehicleResume";
+
+export default function VehiclesPage()
+{
+    return(
+        <div className="flex items-center justify-center">
+            {/* CONTEUDO PARA TESTAR AQ */}
+        </div>
+    );
+}
