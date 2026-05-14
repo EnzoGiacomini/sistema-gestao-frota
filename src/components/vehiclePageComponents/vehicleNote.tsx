ce6611c9cc498cb371d051cde360f6ee98c7a549
Pedro Henrique Mognon De Bortoli <pedro.mbortoli@gmail.com>
2026-05-14 11:35:07 -0300

    refactor: change textarea to inut text

--- src/components/vehiclePageComponents/vehicleNote.tsx
+++ src/components/vehiclePageComponents/vehicleNote.tsx
@@ -2,15 +2,14 @@ export function NoteInput() {
   return (
     <div className="w-full flex flex-col gap-1">
       <label className="text-[15px">Observações</label>
-      <textarea
-        className="border rounded p-1 w-full h-20"
+      <input
+        type="text"
+        className="border rounded p-1 w-full h-20" // Mantive a altura de 20 (h-20)
         id="obs"
         name="obs"
-        rows={5}
-        cols={30}
         maxLength={500}
         placeholder="Deixe uma observação aqui..."
-      ></textarea>
+      />
     </div>
   );
 }
